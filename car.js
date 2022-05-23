c7e5252cc4300a7537ec741c1f6aef04b437c5f7
Mustafa <musgamal05@gmail.com>
2022-05-23 08:25:15 +0200

    collision detectiojn

--- car.js
+++ car.js
@@ -10,14 +10,50 @@ class Car {
         this.maxSpeed = 3;
         this.friction = 0.05;
         this.angle = 0;
+        this.damaged = false;
 
         this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
     update(roadBorders){
-       this.#move();
+        if (!this.damaged){
+            this.#move();
+            this.polygon = this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders);
+        }
        this.sensor.update(roadBorders);
+    }
+
+    #assessDamage(roadBorders){
+        for(let i=0; i<roadBorders.length; i++){
+            if(polyIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+    }
+
+    #createPolygon(){
+        const points = [];
+        const rad = Math.hypot(this.width, this.height)/2;
+        const alpha = Math.atan2(this.width, this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad, 
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad, 
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad, 
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad, 
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        });
+        return points;
     }
 
     #move() {
@@ -63,6 +99,19 @@ class Car {
     }
 
     draw(ctx) {
+        if (this.damaged){
+            ctx.fillStyle="gray";
+        }
+        else {
+            ctx.fillStyle="black";
+        }
+        ctx.beginPath();
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for (let i=1; i<this.polygon.length; i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
+        }
+        ctx.fill();
+        /* 
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(-this.angle)
@@ -76,8 +125,7 @@ class Car {
         ctx.fill();
 
         ctx.restore();
-
-
+        */
         this.sensor.draw(ctx);
     }
 }
